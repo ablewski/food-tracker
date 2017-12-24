55f553c850cbed8e1431802671aa42941d89efbc
Damian Ablewski <ablewskid@gmail.com>
2017-12-24 21:54:23 +0100

    fixed registration

--- key.php
+++ key.php
@@ -1,22 +1,27 @@
 <?php
 
   require_once('config.php');
+  session_start();
 
   if(isset($_SESSION['key'])){
-    unset($_SESSION['key']);
     $bytes = random_bytes(8);
-    $key = hex2bin($bytes);
+    $key = bin2hex($bytes);
     $key_hash = password_hash($key, PASSWORD_DEFAULT);
-    $q = "INSERT INTO users (email, hashed_key) VALUES ('$_SESSION['email']' ,'$key_hash')";
+    $email = $_SESSION['email'];
+    $q = "INSERT INTO users (email, hashed_key) VALUES ('$email' ,'$key_hash')";
     if($c->query($q) === TRUE){
       $_SESSION['success'] = "Registration successfull";
       $_SESSION['show_key'] = $key;
     }else{
       $_SESSION['failed'] = "Unable to register, try again later";
       unset($_SESSION['email']);
+      unset($_SESSION['key']);
     }
   }else{
+    unset($_SESSION['email']);
+    unset($_SESSION['key']);
     header('location: register.php');
+
   }
 
  ?>
@@ -27,7 +32,7 @@
   </head>
   <body>
     <div class='container'>
-      <div class='haeder'>
+      <div class='header' id='to-check'>
         <?php
         if(isset($_SESSION['success'])){
           echo $_SESSION['success'];
@@ -48,7 +53,18 @@
         ?>
       </div>
       <div class='button-field'>
-        <input type='button' value='Continue' onclick="check();">
+        <button onclick="check()">Continue</button>
+        <script>
+        function check(){
+          var str_to_check = document.getElementById('to-check').innerHTML;
+          var success = "successfull";
+          if(str_to_check.includes(success)){
+            window.location.href = 'index.php';
+          }else{
+            window.location.href = 'register.php';
+          }
+        }
+      </script>
       </div>
     </div>
   </body>
